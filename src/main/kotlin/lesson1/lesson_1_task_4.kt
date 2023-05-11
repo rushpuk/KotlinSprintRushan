05c7060ad83219b55e1c5176e60740d1b576862a
administrator <ekzot1kCorporation@gmail.com>
2023-05-11 18:35:38 +0300

    lesson_1_task_4 resolution

---
+++ src/main/kotlin/lesson1/lesson_1_task_4.kt
@@ -0,0 +1,16 @@
+package lesson1
+
+fun main(){
+    val length: Long = 408686000000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+}
