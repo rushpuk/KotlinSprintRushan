1b8df9ddf7b19a6950425ea47516a49776f7418a
administrator <ekzot1kCorporation@gmail.com>
2023-05-16 17:17:25 +0300

    lesson_1_task_5 fix

--- src/main/kotlin/lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -3,5 +3,5 @@ package lesson_1
 fun main(){
     val secondsFly = 6480
     val minutesFly = secondsFly / 60
-    println("$minutesFly:${secondsFly % 100}")
+    println("$minutesFly:${secondsFly % 60}0")
 }
