cbaac43927d6e043eb3134f9fd9039cddee1864d
administrator <ekzot1kCorporation@gmail.com>
2023-05-18 08:24:35 +0300

    lesson_2_task_5 fix

--- src/main/kotlin/lesson_2/lesson_2_task_5.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_5.kt
@@ -6,7 +6,8 @@ import kotlin.math.pow
 fun main(){
     val amount = 70000
     val percent = 16.7
-    val scamBank = amount * (Math.pow((1 + percent / 100), 20.0))
+    val scamBank = amount * ((1 + percent / 100).pow(20.0))
+    val showMe = String.format("%.3f", scamBank)
 
-    println(floor(scamBank * 1000.0) / 1000.0)
+    println(showMe)
 }
